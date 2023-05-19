9aadde748cfbddbaaeeaea7fde2ef359980ef97b
syadav123 <103132639+syadav123@users.noreply.github.com>
2023-05-19 20:56:16 +0530

    Update OtScheduled.sql

--- openmrs/apps/reports/sql/OtScheduled.sql
+++ openmrs/apps/reports/sql/OtScheduled.sql
@@ -1,16 +1,17 @@
-SELECT
-  DISTINCT(sb.start_datetime)                                                AS "Operation Date & Time",
-  sa_timehrs.value                                                           AS "Est. time Hrs",
-  sa_timemint.value                                                          AS "Est. time Mint",
-  l.name                                                                     AS 'Operation Theatre',
-  pi.identifier                                                              AS "Patient Id",
-  concat(pn.given_name, " ", ifnull(pn.family_name, ""))                     AS "Patient Name",
-  concat(pns.given_name, " ", ifnull(pns.family_name, ""))                   AS `Surgeon name`,
-  concat(pno.given_name, " ", ifnull(pno.family_name, ""))                   AS "Other surgeon",
-  sa_assistant.value                                                         AS "Surgical Assistant",
-  sa_Anaesthetist.value                                                      AS "Anaesthetist",
-  sa_Scrub.value                                                             AS "Scrub Nurse",
-  sa_Circulating.value                                                       AS "Circulating Nurse"
+SELECT 
+  DISTINCT 
+  DATE_FORMAT(sb.start_datetime,'%d-%m-%Y %h:%i %p')                         AS 'Operation Date & Time',
+  concat(sa_timehrs.value, ' ')                                              AS 'Est. time Hrs',
+  concat(sa_timemint.value, ' ')                                             AS 'Est. time Mint',
+  concat(l.name, '')                                                         AS 'Operation Theatre',
+  concat(pi.identifier , '')                                                 AS 'Patient ID',
+  concat(pn.given_name, ' ', ifnull(pn.family_name, ''))                     AS 'Patient Name',
+  concat(pns.given_name, ' ', ifnull(pns.family_name, ''))                   AS 'Surgeon Name',
+  concat(pno.given_name, ' ', ifnull(pno.family_name, ''))                   AS 'Other Surgeon',
+  concat(sa_assistant.value, '')                                             AS 'Surgical Assistant',
+  concat(sa_Anaesthetist.value, '')                                          AS 'Anaesthetist',
+  concat(sa_Scrub.value, '')                                                 AS 'Scrub Nurse',
+  concat(sa_Circulating.value, '')                                           AS 'Circulating Nurse'
   FROM surgical_appointment sa
   LEFT JOIN surgical_appointment_attribute sa_timehrs on sa_timehrs.surgical_appointment_id = sa.surgical_appointment_id and sa_timehrs.surgical_appointment_attribute_type_id = 2
   LEFT JOIN surgical_appointment_attribute sa_timemint on sa_timemint.surgical_appointment_id = sa.surgical_appointment_id and sa_timemint.surgical_appointment_attribute_type_id = 3
@@ -24,11 +25,11 @@ SELECT
   JOIN person_name pn ON pn.person_id = sa.patient_id AND pn.voided IS FALSE
   LEFT join provider pro_s ON pro_s.provider_id = sb.primary_provider_id
   LEFT JOIN person_name pns ON pns.person_id = pro_s.person_id AND pns.voided IS FALSE
-  LEFT join provider pro_other_s ON pro_other_s.person_id = sa_other_s.value
+  LEFT join provider pro_other_s ON pro_other_s.provider_id = sa_other_s.value
   LEFT JOIN person_name pno ON pno.person_id = pro_other_s.person_id AND pno.voided IS FALSE
   LEFT JOIN patient_identifier pi ON pi.patient_id = pn.person_id AND pi.voided IS FALSE
   INNER JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id AND
-                                                 pit.name = "Patient Identifier"
+                                                 pit.name = 'Patient Identifier'
   JOIN location l ON sb.location_id = l.location_id AND l.retired IS FALSE
-       WHERE cast(CONVERT_TZ(sa.date_created,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#' AND sa.voided IS FALSE AND sa.status = "SCHEDULED"
+       WHERE cast(CONVERT_TZ(sa.date_created,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#' AND sa.voided IS FALSE AND sa.status = 'SCHEDULED'
         GROUP BY sa.surgical_appointment_id;
