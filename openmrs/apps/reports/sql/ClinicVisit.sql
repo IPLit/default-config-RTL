bd0bff7408b74f5f031d14628fc6c963fa975e1e
syadav123 <103132639+syadav123@users.noreply.github.com>
2023-05-26 10:57:16 +0530

    Update ClinicVisit.sql

--- openmrs/apps/reports/sql/ClinicVisit.sql
+++ openmrs/apps/reports/sql/ClinicVisit.sql
@@ -1,6 +1,7 @@
 SELECT
   DISTINCT(pi.identifier)                                                   AS "Patient Identifier",
   concat(pn.given_name, " ", ifnull(pn.family_name, ""))                    AS "Patient Name",
+  CASE WHEN pa.value='true' then 'نعم' else '' END                          AS "Emergency patient",
   floor(DATEDIFF(DATE(v.date_started), p.birthdate) / 365)                  AS "Age",
   DATE_FORMAT(p.birthdate, "%d-%b-%Y")                                      AS "Birthdate",
   p.gender                                                                  AS "Gender",
@@ -19,14 +20,16 @@ SELECT
   ELSE "Inactive"
   END                                                                       AS "Visit Status"
 FROM visit v
-  join visit_attribute va on va.visit_id and va.attribute_type_id=2
+  left outer join visit_attribute va on va.visit_id=v.visit_id and va.attribute_type_id=2
   JOIN visit_type vt ON v.visit_type_id = vt.visit_type_id
   JOIN person p ON p.person_id = v.patient_id AND p.voided is FALSE
   JOIN patient_identifier pi ON p.person_id = pi.patient_id AND pi.voided is FALSE
-  JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id AND pit.retired is FALSE
+  INNER JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id AND
+                                                 pit.name = 'Patient Identifier'
   JOIN person_name pn ON pn.person_id = p.person_id AND pn.voided is FALSE
   LEFT OUTER JOIN person_address paddress ON p.person_id = paddress.person_id AND paddress.voided is FALSE
   LEFT OUTER JOIN person_attribute pa ON pa.person_id = p.person_id AND pa.voided is FALSE
+                                                                    AND pa.person_attribute_type_id=27
   LEFT OUTER JOIN person_attribute_type pat ON pat.person_attribute_type_id = pa.person_attribute_type_id AND pat.retired is FALSE
   WHERE v.voided is FALSE
   AND cast(CONVERT_TZ(v.date_started,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#'
