dd83cdd513947738d54ef78f90dd414b90b731a7
syadav123 <103132639+syadav123@users.noreply.github.com>
2023-07-08 10:50:34 +0530

    Update countDeath.sql

--- openmrs/apps/reports/sql/countDeath.sql
+++ openmrs/apps/reports/sql/countDeath.sql
@@ -7,4 +7,4 @@ SELECT
      WHEN p.gender='O' then 1 ELSE 0 END)     AS "All"
 FROM person_name pn
 LEFT JOIN person p ON pn.person_id = p.person_id AND p.voided is FALSE AND p.dead=1
-where cast(CONVERT_TZ(p.death_date,'+00:00','+5:30') AS DATE) BETWEEN '2023-06-22' AND '2023-06-22';
+where cast(CONVERT_TZ(p.death_date,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#';
